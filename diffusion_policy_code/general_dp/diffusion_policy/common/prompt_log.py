aa8546389c9fa823db0c54aefa65a2561e89ce32
lyttttt3333 <liyitong21@mails.tsinghua.edu.cn>
2024-11-12 14:19:33 -0500

    modified:   diffusion_policy_code/general_dp/config/pack_battery_attn.yaml 	modified:   diffusion_policy_code/general_dp/diffusion_policy/common/prompt_log.py 	modified:   diffusion_policy_code/robomimic/robomimic/models/pointnet2_utils_dense_sample.py

--- diffusion_policy_code/general_dp/diffusion_policy/common/prompt_log.py
+++ diffusion_policy_code/general_dp/diffusion_policy/common/prompt_log.py
@@ -13,22 +13,3 @@ for idx in tgt_idx:
     tgt_battery_list.append(battery_list[idx]) 
 output_var = tgt_battery_list
 # -------------
-# -------------
-# find_instance_in_category API summary
-# PROMPT
-# The first prompt is #battery outside the crate
-
-# RESPONSE
-[0, 1]
-# -------------
-# -------------
-# detection API summary
-# PROMPT
-# ['battery','slot'] 
-
-slot
-
-# RESPONSE
-slot_list = self.get_obj('slot')
-output_var = slot_list
-# -------------
